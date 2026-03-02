0071027cd135c92fd975edfef2e4ffe12c83ce4a
WEI-TSUNG CHENG <3118197C@student.gla.ac.uk>
2026-03-02 20:51:18 +0000

    1. add admin model 2. add Category and Post model

--- guide/admin.py
+++ guide/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here (after defining them in models.py).
+from guide.models import Category, Post
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "main", "sub")
+    search_fields = ("name", "main", "sub")
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "status", "created_at")
+    list_filter = ("status", "category", "created_at")
+    search_fields = ("title", "description", "address")
